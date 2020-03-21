3e4f7b64feb657ca99735ee9771559d16840369b
Viachaslau Zharko <viachaslau.zharko@gmail.com>
2020-03-21 22:56:01 +0300

    add solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,26 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    const N = 10;
+    let decodedSentence = [];
+    let words = expr.split('*'.repeat(N)); 
+    words.forEach(word => {
+        let decodedWord = '';
+        for (let i = 0; i < word.length; i += N) {
+            let letter = word.substring(i, i + 10); 
+            let decodedLetter = '';
+            for (let j = 0; j < N; j+=2) {
+                let code = letter.substring(j, j + 2); 
+                switch (code) {
+                    case '10': decodedLetter += '.'; break;
+                    case '11': decodedLetter += '-'; break;
+                }
+            }
+            decodedWord += MORSE_TABLE[decodedLetter];
+        }
+        decodedSentence.push(decodedWord);
+    });
+    return decodedSentence.join(' ');
 }
 
 module.exports = {
